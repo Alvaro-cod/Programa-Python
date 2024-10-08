cd97a3d1af7195e5134d4f7be75912d6a7f91536
Alvaro-cod <130327692+Alvaro-cod@users.noreply.github.com>
2024-10-08 16:34:43 -0500

    Update main.py

--- main.py
+++ main.py
@@ -3,10 +3,11 @@ from tren import Tren
 from Pasajero import Pasajero
 from boleto import Boleto
 
+# aqui el numero de paradas 
 def main():
     tren = Tren(paradas=["Estación 1", "Estación 2", "Estación 3"])
     
-    for i in range(1, 4):  # Hay 3 vagones
+    for i in range(1, 4):  # numero de vagones
         tren.agregar_vagon(Vagon(i))
     
     while True:
@@ -23,6 +24,7 @@ def main():
         
         opcion = input("Selecciona una opción: ")
         
+        # pide toda la informacion del pasajero 
         if opcion == "1":
             nombre = input("Ingrese su nombre: ")
             edad = int(input("Ingrese su edad: "))
@@ -34,19 +36,23 @@ def main():
             pasajero = Pasajero(nombre, edad, genero, boleto)
             tren.subir_pasajero(pasajero)
         
+        # al ingresar el numero de una silla baja al pasajero que haya en ella
         elif opcion == "2":
             numero_silla = int(input("Número de silla a liberar: "))
             tren.bajar_pasajero(numero_silla)
         
+        # hace una lista de todos los pasajeros del tren 
         elif opcion == "3":
             pasajeros = tren.listar_pasajeros()
             for pasajero in pasajeros:
                 print(f"Nombre: {pasajero.nombre}, Edad: {pasajero.edad}, Género: {pasajero.genero}, Asiento: {pasajero.boleto.get_numero_asiento()}, Precio: {pasajero.boleto.precio}")
         
+        # hace la cuenta de cuantos pasajeros hay por genero 
         elif opcion == "4":
             conteo = tren.contar_pasajeros_por_genero()
             print(f"Hombres: {conteo['M']}, Mujeres: {conteo['F']}")
         
+        # busca un pasajero por nombre y ofrece la informacion que hay de el
         elif opcion == "5":
             nombre = input("Nombre del pasajero a buscar: ")
             pasajero = tren.buscar_pasajero(nombre)
@@ -55,15 +61,18 @@ def main():
             else:
                 print("Pasajero no encontrado.")
         
+        #  ordena todos los pasajeros por edad de menor a mayor
         elif opcion == "6":
             pasajeros = tren.ordenar_pasajeros_por_edad()
             for pasajero in pasajeros:
                 print(f"Nombre: {pasajero.nombre}, Edad: {pasajero.edad}, Género: {pasajero.genero}, Asiento: {pasajero.boleto.get_numero_asiento()}, Precio: {pasajero.boleto.precio}")
         
+        # calcula el total pagado por los pasajeros
         elif opcion == "7":
             total = tren.calcular_total_pagado()
             print(f"Total pagado: {total}")
         
+        # muestra cuntas personas hay subidad cuantas bajadas y lo que se ha pagado
         elif opcion == "8":
             tren.mostrar_estadisticas()
         
