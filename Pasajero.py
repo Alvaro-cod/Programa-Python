714e9bca1c122e004b5c3d3f258387ab37d43ee2
Alvaro-cod <130327692+Alvaro-cod@users.noreply.github.com>
2024-10-08 16:37:45 -0500

    Update Pasajero.py

--- Pasajero.py
+++ Pasajero.py
@@ -5,14 +5,17 @@ class Pasajero:
         self._genero = genero
         self.boleto = boleto
 
+     # da el nombre del pasajero.
     @property
     def nombre(self):
         return self._nombre
 
+    # nos proporciona su edad
     @property
     def edad(self):
         return self._edad
 
+    # nos proporciona su genero
     @property
     def genero(self):
         return self._genero
@@ -20,6 +23,7 @@ class Pasajero:
     def get_boleto(self):
         return self.boleto
 
+    # da el valor del boleto del pasajero.
     def set_boleto(self, boleto):
         self.boleto = boleto
 
